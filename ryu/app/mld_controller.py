51b9783ba93e62f1f4051a72cbce5f6b920c0c0a
takeuchi satoshi <takeuchi17401@gmail.com>
2014-06-27 13:53:58 +0900

    送信データをpacketからmsgに修正

--- ryu/app/mld_controller.py
+++ ryu/app/mld_controller.py
@@ -2,7 +2,7 @@
 # create_file(zmp tmp file)
 #  /tmp/feeds/0
 #  /tmp/feeds/1
-
+import multiprocessing
 from ryu.ofproto import ofproto_v1_3, ether, inet
 from ryu.lib.packet import packet, ethernet, ipv6, icmpv6, vlan
 from ryu.app import simple_switch_13
@@ -17,12 +17,14 @@ import logging
 import os
 
 
-class mld_controller(simple_switch_13.SimpleSwitch13):
+class mld_controller(simple_switch_13.SimpleSwitch13, multiprocessing.Process):
 
     LOG_LEVEL = logging.DEBUG
 
     # send interval(sec)
     WAIT_TIME = 1
+    SOCKET_TIME_OUT = 1000
+    SOCKET_FLG = 1
 
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
@@ -48,7 +50,6 @@ class mld_controller(simple_switch_13.SimpleSwitch13):
         self.logger.addHandler(stream_log)
         self.logger.setLevel(self.LOG_LEVEL)
         self.logger.debug("")
-        self.logger.debug("[SendSocket]IPC %s", self.IPC_PATH_SEND)
         patcher.monkey_patch()
 
         # ====================================================================
@@ -61,18 +62,14 @@ class mld_controller(simple_switch_13.SimpleSwitch13):
         self.check_exists_tmp(self.RECV_PATH)
 
         ctx = zmq.Context()
-
         # SEND SOCKET CREATE
         self.send_sock = ctx.socket(zmq.PUB)
         self.send_sock.bind(self.IPC_PATH_SEND)
-        self.logger.debug("[SendSocket]IPC %s", self.IPC_PATH_SEND)
 
         # RECV SOCKET CREATE
         self.recv_sock = ctx.socket(zmq.SUB) 
         self.recv_sock.connect(self.IPC_PATH_RECV)
         self.recv_sock.setsockopt(zmq.SUBSCRIBE, "")
-        self.logger.debug("[RecvSocket]IPC %s", self.IPC_PATH_RECV)
-
         recv_thread = self.org_thread.Thread(
                                     target=self.receive_from_mld,
                                     name="ReceiveThread")
@@ -88,7 +85,7 @@ class mld_controller(simple_switch_13.SimpleSwitch13):
 
         msg = ev.msg
         pkt = packet.Packet(msg.data)
-
+        """
         # CHECK ETH
         pkt_ethernet = pkt.get_protocol(ethernet.ethernet)
         if not pkt_ethernet:
@@ -103,10 +100,10 @@ class mld_controller(simple_switch_13.SimpleSwitch13):
         if not pkt_icmpv6.type_ in [icmpv6.MLDV2_LISTENER_REPORT,
                                     icmpv6.ICMPV6_MEMBERSHIP_QUERY]:
             return
+        """
+        self.logger.debug("###send packet= %s \n", str(pkt))
 
-        self.logger.debug("packet-in %s \n", (pkt))
-
-        self.send_packet_to_mld(pkt)
+        self.send_to_mld(msg)
 
     # =========================================================================
     # _switch_features_handler
@@ -119,14 +116,14 @@ class mld_controller(simple_switch_13.SimpleSwitch13):
         self.datapath = ev.msg.datapath
 
     # =========================================================================
-    # send_packet_to_mld
+    # send_to_mld
     # =========================================================================
-    def send_packet_to_mld(self, sendpkt):
+    def send_to_mld(self, sendmsg):
         self.logger.debug("")
 
         # send of zeromq
-        self.send_sock.send(cPickle.dumps(sendpkt, protocol=0))
-        self.logger.info("sent 1 packet to mld_process.")
+        self.send_sock.send(cPickle.dumps(sendmsg, protocol=0))
+        self.logger.info("sent 1 to mld_process.")
 
     # =========================================================================
     # receive_from_mld
@@ -138,7 +135,7 @@ class mld_controller(simple_switch_13.SimpleSwitch13):
             # receive of zeromq
             recvpkt = self.recv_sock.recv()
             packet = cPickle.loads(recvpkt)
-            self.logger.debug("####recvpkt= %s \n", str(packet))
+            self.logger.debug("###recv packet= %s \n", str(packet))
             self.send_packet_out(packet)
             self.org_thread_time.sleep(self.WAIT_TIME)
 
@@ -157,7 +154,7 @@ class mld_controller(simple_switch_13.SimpleSwitch13):
         src = pkt_eth.src
         self.mac_to_port.setdefault(datapath, {})
 
-        self.logger.debug("packet out %s %s %s", datapath, src, dst)
+        self.logger.debug("packet out %s %s %s \n", datapath.id, src, dst)
 
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
                                           ofproto.OFPCML_NO_BUFFER)]
