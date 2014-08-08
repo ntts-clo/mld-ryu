3e829c6966d3451105d75defba6620d47e545ea7
matsumoto shingo <matsumoto.shingo.z02@gmail.com>
2014-08-08 13:13:14 +0900

    PEP8対応

--- mld/apptest/mldv2_report_sender.py
+++ mld/apptest/mldv2_report_sender.py
@@ -40,7 +40,8 @@ class mldv2_report_sender(object):
     def send_add(self, num_of=3000):
 
         record_list = []
-        for report_type in (icmpv6.ALLOW_NEW_SOURCES, icmpv6.CHANGE_TO_INCLUDE_MODE):
+        for report_type in (icmpv6.ALLOW_NEW_SOURCES,
+                            icmpv6.CHANGE_TO_INCLUDE_MODE):
             record_list.append(icmpv6.mldv2_report_group(type_=report_type,
                                                          address=MC_ADDRESS,
                                                          srcs=[SERV_IP]))
