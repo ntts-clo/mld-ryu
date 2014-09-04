338232c3b49e03ac5a03920e89263644cb0f6865
takeuchi satoshi <takeuchi17401@gmail.com>
2014-09-04 16:04:34 +0900

    未使用関数__getitem__の削除

--- common/read_json.py
+++ common/read_json.py
@@ -6,6 +6,3 @@ class read_json():
     def __init__(self, filename):
         jsonfile = open(filename)
         self.data = json.load(jsonfile)
-
-    def __getitem__(self, key):
-        return self.data[key]
